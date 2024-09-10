0ff757f86a2a9809f833fb30c700e0ea4332f9c5
DaniaGaGz <daniagagz16@gmail.com>
2024-09-10 12:06:16 -0600

    Delete import random.py

--- import random.py
+++
@@ -1,9 +0,0 @@
-import random
-# List of words to choose from
-word_list = ['python', 'java', 'javascript', 'ruby', 'html', 'css']
-# Randomly select a word from the list
-selected_word = random.choice(word_list)
-# Initialize the user's guess
-user_guess = ""
-while user_guess != selected_word:
-    print("you failed")
