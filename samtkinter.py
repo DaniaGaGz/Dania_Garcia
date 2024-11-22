b724818d569e309a5c1c6a5506736404c415efb0
DaniaGaGz <daniagagz16@gmail.com>
2024-11-22 09:02:07 -0600

    tkinter

---
+++ samtkinter.py
@@ -0,0 +1,17 @@
+import tkinter
+
+m = tkinter.Tk()
+
+m.title("The informatica teacher is the best. ")
+
+
+
+
+fn_label = tkinter(m, text ="First name: ")
+fn_label.grid(row=0)
+ln_label = tkinter()
+ln_label = tkinter.Label(m, text="Male", variable=male).grid(row=3, sticky=tkinter.W)
+ 
+
+
+
